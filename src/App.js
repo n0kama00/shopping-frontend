af70f1c4d6e69eb58af5ee32e6c7ac44ef9b79df
Emilia Kaihua <n0kama00@students.oamk.fi>
2021-11-08 12:19:08 +0200

    koodit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,85 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const URL = 'http://localhost/shoppinglist/';
 
 function App() {
+
+  const [items, setItems] = useState([]);
+  const [item, setItem] = useState('');
+  const [amounts, setAmounts] = useState([]);
+  const [amount, setAmount] = useState('');
+
+  useEffect(() => {
+    axios.get(URL)
+    .then((response) => {
+      setItems(response.data);
+      setAmounts(response.data);
+    }).catch(error => {
+      alert(error.response ? error.response.data.error : error);
+    })
+  }, []);
+
+  function save(e) {
+    e.preventDefault();
+    const json = JSON.stringify({ description: item, amount: amount })
+    axios.post(URL + 'save.php', json, {
+      headers: {
+        'Content-Type' : 'application/json'
+      }
+    })
+    .then((response) => {
+      console.log(response);
+      setItems(items => [...items, response.data]);
+      setItem('');
+      setAmounts(amounts => [...amounts, response.data]);
+      setAmount('');
+    }).catch (error => {
+      alert(error.response.data.error)
+    });
+  }
+
+  function remove(id) {
+    const json = JSON.stringify({ id: id })
+    axios.post(URL + 'remove.php', json, {
+      headers: {
+        'Content-type': 'application/json'
+      }
+    })
+      .then((response) => {
+        console.log(response);
+        const newListWithoutRemoved = items.filter((item) => item.id !== id);
+        const newAmountListWithoutRemoved = amounts.filter((item) => item.id !== id);
+        setItems(newListWithoutRemoved);
+        setAmounts(newAmountListWithoutRemoved);
+      }).catch(error => {
+        alert(error.response ? error.response.data.error : error);
+      });
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="container">
+    <h3> Shopping list </h3>
+    <form onSubmit={save}>
+      <label> New item </label>
+      <input value={item} onChange={e => setItem(e.target.value)} placeholder="Item"/>
+      <input value={amount} onChange={e => setAmount(e.target.value)} placeholder="Amount"/>
+      <button> Add </button>
+    </form>
+    <ul>
+      {items?.map(item => (
+        <li key={item.id}>{item.description}</li>
+      ))}
+    </ul>
+    <ul>
+      {amounts?.map(amount => (
+        <li key={amount.id}>{amount.amount}
+          <a href="#" className="delete" onClick={() => remove(amount.id)}>Delete</a>
+        </li>
+      ))}
+    </ul>   
+  </div>
   );
 }
 
